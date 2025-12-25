7b729d8e0d918300c42b3d278f6d5b50484d05fe
HappyLadySauce <13452552349@163.com>
2025-12-25 09:17:14 +0800

    Remove AllowOriginFunc from CORS middleware to simplify configuration and enhance security by eliminating specific origin checks.

--- cmd/app/middleware/cors.go
+++ cmd/app/middleware/cors.go
@@ -23,9 +23,6 @@ func Cors() gin.HandlerFunc {
 		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type", "Accept"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://github.com"
-		},
 		MaxAge: maxAge * time.Hour,
 	})
 }
