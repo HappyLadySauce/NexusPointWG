e8bb333cc27a959e3518f69e5aec6ce15766dcbe
HappyLadySauce <13452552349@163.com>
2025-12-30 15:25:19 +0800

    refactor: 移除不再使用的local和wireguard模块代码
    删除了与local和wireguard模块相关的所有代码文件，进一步清理项目结构，提升代码可维护性。

--- internal/pkg/core/local/store.go
+++
@@ -1,7 +0,0 @@
-package local
-
-// LocalStore defines storage operations for local file system.
-type LocalStore interface {
-	UserConfigStore() UserConfigStore
-	ServerConfigStore() ServerConfigStore
-}
